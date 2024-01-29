b44444053c9239a4f1ee23916dbbc1fdccad8365
deepanshuchandok <49022115+deepanshuchandok@users.noreply.github.com>
2024-01-29 05:56:03 +0530

    Create action-Script.sh

---
+++ action-Script.sh
@@ -0,0 +1,18 @@
+          echo what is the direcory 
+          ls -a
+          echo is java installed ?
+          java -version 
+          echo is git installed ?
+          git --version 
+          echo what about the build tolls version ?
+          mvn --version 
+          gradle --version 
+          ant -version
+          echo what is the enviornment ?
+          env 
+          echo what is my work directory ?
+          echo $RUNNER_WORKSPACE
+          echo who am I ?
+          whoami
+          echo how is disk laid ?
+          df
